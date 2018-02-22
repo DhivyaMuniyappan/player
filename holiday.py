38c8e20dd56166974ceccbdf53bf67fea97a5912
DhivyaMuniyappan <36299454+DhivyaMuniyappan@users.noreply.github.com>
2018-02-22 17:07:43 +0530

    holiday

---
+++ holiday.py
@@ -0,0 +1,12 @@
+x=int(input("enter your day:"))
+sunday=0
+monday=1
+tuesday=2
+wednesday=3
+thursday=4
+friday=5
+saturday=6
+if(x==0 or x==6):
+  print("Holiday")
+else:
+  print("Not a holiday")
