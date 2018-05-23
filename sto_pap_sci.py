523691e1dcb273e97421581bd6569c6d7144c8ce
DhivyaMuniyappan <36299454+DhivyaMuniyappan@users.noreply.github.com>
2018-05-23 16:16:55 +0530

    sto_pap_sci

---
+++ sto_pap_sci.py
@@ -0,0 +1,18 @@
+P='paper'
+R='Rock'
+S='Scissor'
+user1,user2=input('').split(" ")
+if(user1=='P' and user2=='R'):
+  print('P')
+elif(user1=='P' and user2=='S' ):
+  print('S')
+elif(user1=='R' and user2=='P' ):
+  print('P')
+elif(user1=='R' and user2=='S' ):
+  print('R')
+elif(user1=='S' and user2=='P' ):
+  print('S')
+elif(user1=='S' and user2=='R'):
+  print('R')
+else:
+  print('Invalid')
